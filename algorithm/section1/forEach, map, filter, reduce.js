626c4104f84bfa7782cdde6617299f96e55c93af
yeonkr <1714882@sookmyung.ac.kr>
2023-01-18 20:57:25 +0900

    filter

--- algorithm/section1/forEach, map, filter, reduce.js
+++ algorithm/section1/forEach, map, filter, reduce.js
@@ -44,3 +44,22 @@ let result = a.map(function (v, i) {
   return v * v; // 배열의 요소를 제곱한 새로운 배열을 반환한다.
 });
 console.log(result); // [ 100, 121, 144, 169, 196, 225 ] 제곱한 값이 들어있는 배열이 반환된다.
+
+//--------------------------------------------------------------
+// filter
+// 배열의 요소를 하나하나 탐색하며, 조건에 맞는 요소만 모아 새로운 배열을 만들어 반환한다.
+// 즉, 배열의 요소를 걸러낼 때 사용한다.
+// filter는 새로운 배열을 반환하며 원본배열과 길이가 다를 수 있다.(특정 값만 모아서 뽑아냄)
+function filter(predicate, thisArg) {
+  let list = [];
+  for (let i = 0; i < a.length; i++) {
+    // 배열의 요소를 하나하나 탐색하며
+    if (predicate(a[i], i)) list.push(a[i]);
+  } // predicate의 조건에 맞는 참인 요소만 모아서 a[i]를 새로운 배열에 push한다.
+  return list;
+}
+a = [10, 11, 12, 13, 14, 15];
+let answer = a.filter(function (v, i) {
+  return v % 2 === 1;
+});
+console.log(answer); // [ 11, 13, 15 ] 홀수만 모아서 새로운 배열을 반환한다.

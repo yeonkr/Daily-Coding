65c10167ed4f8dd7a8bdbfed8cd9d075518f9714
yeonkr <1714882@sookmyung.ac.kr>
2023-05-19 16:59:32 +0900

    격자판 최대합

---
+++ algorithm/section2/6. 격자판 최대합.js
@@ -0,0 +1,62 @@
+/* 
+N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력합니다.
+
+▣ 입력설명
+첫 줄에 자연수 N이 주어진다.(1<=N<=50)
+두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
+
+▣ 출력설명 
+최대합을 출력합니다.
+
+▣ 입력예제 1
+5
+10 13 10 12 15 
+12 39 30 23 11 
+11 25 50 53 15 
+19 27 29 37 27 
+19 13 30 13 19
+
+▣ 출력예제 1 
+155
+*/
+
+function solution(arr) {
+  const n = arr.length;
+  let maxSum = 0;
+
+  // 각 행과 열의 합 계산
+  for (let i = 0; i < n; i++) {
+    let rowSum = 0;
+    let colSum = 0;
+    for (let j = 0; j < n; j++) {
+      rowSum += arr[i][j];
+      colSum += arr[j][i];
+    }
+    maxSum = Math.max(maxSum, rowSum, colSum);
+  }
+
+  // 첫 번째 대각선의 합 계산
+  let diagSum1 = 0;
+  for (let i = 0; i < n; i++) {
+    diagSum1 += arr[i][i];
+  }
+  maxSum = Math.max(maxSum, diagSum1);
+
+  // 두 번째 대각선의 합 계산
+  let diagSum2 = 0;
+  for (let i = 0; i < n; i++) {
+    diagSum2 += arr[i][n - 1 - i];
+  }
+  maxSum = Math.max(maxSum, diagSum2);
+
+  return maxSum;
+}
+
+let arr = [
+  [10, 13, 10, 12, 15],
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
+console.log(solution(arr)); // 155

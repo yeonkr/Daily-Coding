a28d8a139a408c7c8ed5e1b16314b6b3d2785605
yeonkr <1714882@sookmyung.ac.kr>
2023-05-21 23:49:14 +0900

    격자판 최대합

--- algorithm/section2/6. 격자판 최대합.js
+++ algorithm/section2/6. 격자판 최대합.js
@@ -60,3 +60,26 @@ let arr = [
   [19, 13, 30, 13, 19],
 ];
 console.log(solution(arr)); // 155
+
+// 강의
+function solution2(arr) {
+  let answer = Number.MIN_SAFE_INTEGER,
+    n = arr.length;
+  let sum1 = (sum2 = 0);
+  for (let i = 0; i < n; i++) {
+    sum1 = sum2 = 0;
+    for (let j = 0; j < n; j++) {
+      sum1 += arr[i][j]; // 행의 합
+      sum2 += arr[j][i]; // 열의 합
+    }
+    answer = Math.max(answer, sum1, sum2);
+  }
+  sum1 = sum2 = 0;
+  for (let i = 0; i < n; i++) {
+    sum1 += arr[i][i]; // 첫 번째 대각선의 합
+    sum2 += arr[i][n - i - 1]; // 두 번째 대각선의 합
+  }
+  answer = Math.max(answer, sum1, sum2);
+  return answer;
+}
+console.log(solution2(arr)); // 155
